09bdc27bcc993c296e639d2e13d6bca422b40041
cristianovisk <cristianovisk@gmail.com>
2019-08-07 23:23:57 -0300

    Update timestamp.py

--- timestamp.py
+++ timestamp.py
@@ -7,7 +7,12 @@ import sys
 #reverseHex = hex[::-1]
 #bin(int("%s" %reverseHex[0]), 16)[2::]
 #binario = bin(int("%s%s%s%s" %(reverseHex[0],reverseHex[1],reverseHex[2],reverseHex[3]), 16))[2:]
-binario=bin(int('1'+sys.argv[1], 16))[3:]
+hexArg=sys.argv[1]
+hexLittleEndian=hexArg[6:8],hexArg[4:6],hexArg[2:4],hexArg[0:2]
+hexBigEndian=(''.join(hexLittleEndian))
+
+
+binario=bin(int('1'+hexBigEndian, 16))[3:]
 
 anobin=binario[0:6]
 ano=int(anobin, 2)
@@ -21,9 +26,10 @@ minutobin=binario[20:26]
 minuto=int(minutobin, 2)
 segundobin=binario[26:32]
 segundo=int(segundobin, 2)
-print ("-----\nBINARIO: %s\n" %binario)
-print ("ANO: %i - BIN ANO: %s \nMES: %i - BIN MES: %s\nDIA: %i - BIN DIA: %s\nHORA: %i - BIN HORA: %s\nMINUTO: %i - BIN MINUTO: %s\nSEGUNDO: %i - BIN SEGUNDO: %s" %(ano,anobin,mes,mesbin,dia,diabin,hora,horabin,minuto,minutobin,segundo,segundobin))
-print ("TIME FINAL: %s/%s/%s as %s:%s:%s" %(dia,mes,ano,hora,minuto,segundo))
+#print ("-----\nBINARIO: %s\n" %binario)
+#print ("ANO: %i - BIN ANO: %s \nMES: %i - BIN MES: %s\nDIA: %i - BIN DIA: %s\nHORA: %i - BIN HORA: %s\nMINUTO: %i - BIN MINUTO: %s\nSEGUNDO: %i - BIN SEGUNDO: %s" %(ano,anobin,mes,mesbin,dia,diabin,hora,horabin,minuto,minutobin,segundo,segundobin))
+#print ("TIME FINAL: %s/%s/%s as %s:%s:%s" %(dia,mes,ano,hora,minuto,segundo))
+print ("d%s-m%s-a%s_h%s-m%s-s%s" %(dia,mes,ano,hora,minuto,segundo))
 
 
 #bin(int("45a15d84", 16))[2:]
