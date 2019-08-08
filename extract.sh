8d5d7e1750da9b5ec82b4f9dfd5e3c20e3158416
cristianovisk <cristianovisk@gmail.com>
2019-08-08 00:44:47 -0300

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -6,7 +6,7 @@
 
 fileDD=$1
 dhavMagicNumber="dhav	y	20000000	\x44\x48\x41\x56	\x64\x68\x61\x76"
-folder="DHAV_Extracted_$RANDOM"
+folder="DHAV_Extracted_$fileDD"
 
 function main {
 	if [ `whoami` != "root" ];
@@ -47,10 +47,12 @@ function extractDHAV {
 	clear
 	printf "Extraindo..."
 	foremost -o $folder $fileDD
+	sync
 	cd $folder/dhav
 	clear
 	echo "Extraido `ls | wc -l` Frames em $folder - Tratando..."
 	sleep 1
+	echo "FILENAME;HEADER;IDSEQ;TYPEFRAME;NUMCAM;DIA;MES;ANO;HORA;MINUTO;SEGUNDO" > ../../report_images_$fileDD.csv
 	for file in `ls`;
 	do
 		header=$(cat $file | hexdump -ve '16/1 "%02x"' | cut -c1-44)
@@ -58,7 +60,8 @@ function extractDHAV {
 		typeFrame=$(echo ${header^^} | cut -c9-10)
 		numCamFrame=$(echo `echo ${header^^} | cut -c13-14 | bc`+1 | bc)
 		timestamp=$(echo ${header^^} | cut -c33-40)
-		filename=$(echo CAM$numCamFrame-`python -c "hexArg='$idSeq';hexLittleEndian=hexArg[6:8],hexArg[4:6],hexArg[2:4],hexArg[0:2];hexBigEndian=(''.join(hexLittleEndian));print(int(hexBigEndian, 16))"`-`python ../../timestamp.py $timestamp`-$typeFrame.dat)
+		filename=$(echo CAM$numCamFrame-`python -c "hexArg='$idSeq';hexLittleEndian=hexArg[6:8],hexArg[4:6],hexArg[2:4],hexArg[0:2];hexBigEndian=(''.join(hexLittleEndian));print(int(hexBigEndian, 16))"`-`python ../../timestamp.py $timestamp 1`-$typeFrame.dat)
+		echo "$filename;$header;`python -c "hexArg='$idSeq';hexLittleEndian=hexArg[6:8],hexArg[4:6],hexArg[2:4],hexArg[0:2];hexBigEndian=(''.join(hexLittleEndian));print(int(hexBigEndian, 16))"`;$typeFrame;$numCamFrame;`python ../../timestamp.py $timestamp 2`" >> ../../report_images_$fileDD.csv
 		echo $filename
 		if [[ -d CAM$numCamFrame ]];
 		then
