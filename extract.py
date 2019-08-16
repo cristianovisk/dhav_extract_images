6e9941e8f12f4eb7e2c68d1b816458283e754329
cristianovisk <cristianovisk@gmail.com>
2019-08-15 22:51:19 -0300

    Update extract.py

--- extract.py
+++ extract.py
@@ -3,7 +3,11 @@
 import sys
 import os
 import binascii
-import time 
+import time
+from datetime import datetime, time
+now = datetime.now()
+beginning_of_day = datetime.combine(now.date(), time(0))
+print (now - beginning_of_day)
 
 arq=sys.argv[1]
 if os.path.isdir("%s_Extracted" %arq) == False: 
@@ -12,26 +16,7 @@ else:
     os.system("rm -rf %s_Extracted" %arq)
     os.system("mkdir %s_Extracted" %arq)
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
-
+#Transforma Hex em Binario(string) depois processa o Timestamp
 def timestamp_translate(timestamp):
     hexArg=timestamp
     hexLittleEndian=hexArg[6:8],hexArg[4:6],hexArg[2:4],hexArg[0:2]
@@ -49,7 +34,10 @@ def timestamp_translate(timestamp):
     horabin=binario[15:20]
     hora=int(horabin, 2)
     minutobin=binario[20:26]
-    minuto=int(minutobin, 2)
+    if minutobin.count('0') != 0:
+        minuto=int(minutobin, 2)
+    elif minutobin.count('0') == 0:
+        minuto=int(0)
     segundobin=binario[26:32]
     if segundobin.count('0') != 0:
         segundo=int(segundobin, 2)
@@ -66,53 +54,36 @@ def timestamp_translate(timestamp):
         "segundo": segundo
     }
     return timedict
-    #print ("d%s-m%s-a%s_h%s-m%s-s%s" %(dia,mes,ano,hora,minuto,segundo))
 
 mDHAV = b'\x44\x48\x41\x56'
 fDHAV = b'\x64\x68\x61\x76'
 filesize = os.path.getsize(arq)
 with open(arq, "rb") as bytes:
-    #bytes.seek(13,0)
-    #print(binascii.b2a_hex(bytes.read(1)))
-    #printProgressBar(0, filesize, prefix = 'Progress:', suffix = 'Complete', length = 50)
     for byte in range(filesize):
-        #printProgressBar(byte, filesize, prefix = 'Progress:', suffix = 'Complete', length = 50)
         bytes.seek(byte, 0)
         offset = bytes.read(4)
         if mDHAV == offset:
             header=byte
-            #print("Match DHAV Frame")
-            #print(byte)
             #Tipo de Frame Primário ou Dependente FD/FC/F1 respectivamente
             bytes.seek(byte+4,0)
             typeframe = bytes.read(1)
-            #print("Tipo de Frame: %s" %typeframe)
             #Numero do Canal/Camera do Frame
             bytes.seek(byte+6,0)
             camNumber = int(binascii.b2a_hex(bytes.read(1)), 16)+1
-            #print("Camera: %s" %camNumber)
             folderCam=os.path.isdir("%s_Extracted/CAM%s" %(arq, camNumber))
             if folderCam == False:
                 os.system("mkdir %s_Extracted/CAM%s" %(arq, camNumber))
             #Numero Sequencial do Frame
             bytes.seek(byte+8,0)
-            seqFrame = int.from_bytes(bytes.read(4), byteorder='little')
-            #print("Numero Frame: %i" %seqFrame)
+            seqFrame = int.from_bytes(bytes.read(4), byteorder='little'))
             #Tamanho em Bytes do Frame
             bytes.seek(byte+12,0)
             sizeFrame = int.from_bytes(bytes.read(4), byteorder='little')
-            #print("Tamanho Frame: %i Bytes" %sizeFrame)
             #Timestamp do Frame
             bytes.seek(byte+16,0)
             timestamp = int.from_bytes(bytes.read(4), byteorder='big')
-
             timestamp = timestamp_translate(str(hex(timestamp)).replace('0x', ''))
-            '''#Footer do Arquivo
-            bytes.seek(byte+sizeFrame-8,0)
-            footerFrame = bytes.read(4)
-            if footerFrame != fDHAV:
-                print("Footer: %s" %footerFrame)
-                '''
+
         if fDHAV == offset:
             footer=byte
             try:
@@ -129,3 +100,7 @@ with open(arq, "rb") as bytes:
                     del footer
             else:
                 header=0
+                
+    now = datetime.now()
+    beginning_of_day = datetime.combine(now.date(), time(0))
+    print (now - beginning_of_day)
